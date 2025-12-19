d70fcfbe63f302a4d3b859d7e7b712670c9e1cf8
FRANCESCO DAMINELLI <daminelli.francesco.07@itisdalmine.edu.it>
2025-12-19 09:26:57 +0100

    creati file sql per creazione tabelle e la popolazione di queste

---
+++ tables.sql
@@ -0,0 +1,51 @@
+create table evento(
+
+);
+
+create table spettatori(
+
+);
+
+create table tornei(
+
+);
+
+create table giochi(
+
+);
+
+create table squadre(
+
+);
+
+create table membri(
+
+);
+
+create table ruoli(
+
+);
+
+create table sponsor(
+
+);
+
+create table membri_ruoli(
+
+);
+
+create table evento_sponsor(
+
+);
+
+create table evento_spettatori(
+
+);
+
+create table tornei_squadre(
+
+);
+
+create table giochi_membri(
+
+);
